7539f374f4030bf0d88661ce972e45782861a095
AliShiyyab <alishiyyab1998@gmail.com>
2021-08-05 19:16:07 -0700

    Stack and Queue Code Challenge

---
+++ CC10/app/src/main/java/CC10/Node.java
@@ -0,0 +1,22 @@
+package CC10;
+
+public class Node<N> {
+    private Node<N> next;
+    private N data;
+
+    public Node(N data){
+        this.data=data;
+    }
+
+    public N getData() {
+        return data;
+    }
+
+    public Node<N> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<N> next) {
+        this.next = next;
+    }
+}
