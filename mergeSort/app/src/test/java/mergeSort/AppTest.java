e29b0c36a47aa6a960b3ab28c872765d677f1a8b
AliShiyyab <alishiyyab1998@gmail.com>
2021-09-02 12:03:05 +0300

    AA

--- mergeSort/app/src/test/java/mergeSort/AppTest.java
+++ mergeSort/app/src/test/java/mergeSort/AppTest.java
@@ -4,11 +4,17 @@
 package mergeSort;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        int[] arr = {8,4,23,42,16,15};
+        int[] newArr = {4 , 8 , 15 , 16 , 23 , 42};
+
+        assertEquals(Arrays.toString(newArr) , Arrays.toString(classUnderTest.margeSort(arr)));
     }
 }
